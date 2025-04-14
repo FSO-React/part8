2c2475c42e090d0d423ec2acc46e020e2835205b
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-04-13 23:26:00 -0300

    exercise 8.2 and 8.3

--- library/backend/index.js
+++ library/backend/index.js
@@ -6,9 +6,26 @@ const { v1: uuid } = require('uuid');
 const { authors, books } = require('./data/index')
 
 const typeDefs = `
+  type Author {
+    name: String!
+    bookCount: Int!
+    born: Int!
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
     authorCount: Int!
     bookCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -16,7 +33,21 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-  }
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  // Book: {
+  //   author: (root) => {
+  //     const author = authors.find(a => a.name === root.author)
+  //     return author.name
+  //   }
+  // },
+  Author: {
+    bookCount: (root) => {
+      booksOfAuthor = books.filter(b => b.author === root.name)
+      return booksOfAuthor.length
+    }
+  },
 }
 
 const server = new ApolloServer({
