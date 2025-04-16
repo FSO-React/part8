9e8b14c180a0c00ec733d4060b175ea85d7bc8c4
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-04-15 21:01:36 -0300

    exercise 8.10

--- library/frontend/src/components/NewBook.jsx
+++ library/frontend/src/components/NewBook.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_BOOKS, CREATE_BOOK } from '../queries/books'
+import { ALL_AUTHORS } from '../queries/authors'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,15 +10,32 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+  })
+
+  // eslint-disable-next-line react/prop-types
   if (!props.show) {
     return null
   }
 
   const submit = async (event) => {
     event.preventDefault()
+    
+    if (!title || !author || !published || genres.length === 0) {
+      console.error('title, author, published and genres are required')
+      console.log(title, author, published, genres)
+      return
+    }
 
-    console.log('add book...')
-
+    const publishedInt = parseInt(published)
+    await createBook({ variables: { 
+        title: title, 
+        author: author, 
+        published: publishedInt,
+        genres: genres 
+      } 
+    })
     setTitle('')
     setPublished('')
     setAuthor('')
@@ -24,7 +44,9 @@ const NewBook = (props) => {
   }
 
   const addGenre = () => {
-    setGenres(genres.concat(genre))
+    if (!genres.includes(genre) && genre !== '') {
+      setGenres(genres.concat(genre))
+    }
     setGenre('')
   }
 
