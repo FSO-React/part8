db5692d8bd432d6010fed2ad6882eb91cf466367
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-05-01 11:34:02 -0300

    fragments

--- apollo-client/src/queries.js
+++ apollo-client/src/queries.js
@@ -1,27 +1,33 @@
 import { gql } from '@apollo/client'
 
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone 
+    address {
+      street 
+      city
+    }
+  }
+`
+
 export const ALL_PERSONS = gql`
   query  {
     allPersons  {
-      name
-      phone
-      id
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `
 
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone 
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `
 
 export const CREATE_PERSON = gql`
@@ -34,11 +40,11 @@ mutation createPerson($name: String!, $street: String!, $city: String!, $phone: 
   ) {
     name
     phone
-    id
     address {
       street
       city
     }
+    id
   }
 }
 `
@@ -46,15 +52,10 @@ mutation createPerson($name: String!, $street: String!, $city: String!, $phone: 
 export const EDIT_NUMBER = gql`
   mutation editNumber($name: String!, $phone: String!) {
     editNumber(name: $name, phone: $phone)  {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `
 
 export const LOGIN = gql`
